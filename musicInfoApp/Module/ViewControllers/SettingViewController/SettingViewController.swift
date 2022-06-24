62cd76e6bb2b685cd41f7843c48b919a85e8c8b8
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2022-06-24 13:16:21 +0300

    feat: Create SettingViewContoller

---
+++ musicInfoApp/Module/ViewControllers/SettingViewController/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  musicInfoApp
+//
+//  Created by Alexander Pavlovets on 24.06.2022.
+//
+
+import UIKit
+
+//MARK: - TO-DO
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
