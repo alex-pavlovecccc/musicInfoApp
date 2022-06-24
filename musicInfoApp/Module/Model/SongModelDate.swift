bc402435b416d6f5a2b1ffd5dff86fb6874f0380
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2022-06-24 13:35:40 +0300

    feat: fix SongModelDate and add Decoder

--- musicInfoApp/Module/Model/SongModelDate.swift
+++ musicInfoApp/Module/Model/SongModelDate.swift
@@ -15,73 +15,41 @@ struct SearchResponse: Codable {
 
 // MARK: - Result
 struct Song: Codable {
-    let wrapperType: WrapperType
-    let kind: Kind?
-    let artistID, collectionID, trackID: Int?
-    let artistName: String
-    let collectionName, trackName, collectionCensoredName, trackCensoredName: String?
-    let artistViewURL, collectionViewURL, trackViewURL: String?
-    let previewURL: String
-    let artworkUrl30: String?
-    let artworkUrl60, artworkUrl100: String
-    let collectionPrice: Double
-    let trackPrice, collectionHDPrice, trackHDPrice: Double?
-    let releaseDate: Date
-    let collectionExplicitness: Explicitness
-    let trackExplicitness: Explicitness?
-    let discCount, discNumber, trackCount, trackNumber: Int?
-    let trackTimeMillis: Int?
-    let country: Country
-    let currency: Currency
-    let primaryGenreName: String
-    let contentAdvisoryRating, shortDescription, longDescription: String?
-    let isStreamable: Bool?
-    let collectionArtistName: String?
-    let trackRentalPrice, trackHDRentalPrice: Double?
-    let hasITunesExtras: Bool?
-    let resultDescription: String?
+    var artistName: String?
+    var collectionName: String?
+    var trackName: String?
+    var artworkUrl160: URL?
+    var previewUrl: String?
+    var primaryGenreName: String?
+    var trackTimeMillis: Int?
+    var trackId: Int?
+    var largeImage: URL?
 
     enum CodingKeys: String, CodingKey {
-        case wrapperType, kind
-        case artistID = "artistId"
-        case collectionID = "collectionId"
-        case trackID = "trackId"
-        case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
-        case artistViewURL = "artistViewUrl"
-        case collectionViewURL = "collectionViewUrl"
-        case trackViewURL = "trackViewUrl"
-        case previewURL = "previewUrl"
-        case artworkUrl30, artworkUrl60, artworkUrl100, collectionPrice, trackPrice
-        case collectionHDPrice = "collectionHdPrice"
-        case trackHDPrice = "trackHdPrice"
-        case releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, contentAdvisoryRating, shortDescription, longDescription, isStreamable, collectionArtistName, trackRentalPrice
-        case trackHDRentalPrice = "trackHdRentalPrice"
-        case hasITunesExtras
-        case resultDescription = "description"
+        case artistName = "artistName"
+        case collectionName = "collectionName"
+        case trackName = "trackName"
+        case artworkUrl160 = "artworkUrl160"
+        case previewUrl = "previewUrl"
+        case primaryGenreName = "primaryGenreName"
+        case trackTimeMillis = "trackTimeMillis"
+        case trackId = "trackId"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.artistName = try container.decode(String.self, forKey: .artistName)
+        self.collectionName = try container.decode(String.self, forKey: .collectionName)
+        self.trackName = try container.decode(String.self, forKey: .trackName)
+        self.previewUrl = try container.decode(String.self, forKey: .previewUrl)
+        self.primaryGenreName = try container.decode(String.self, forKey: .primaryGenreName)
+        self.trackTimeMillis = try container.decode(Int.self, forKey: .trackTimeMillis)
+        self.trackId = try container.decode(Int.self, forKey: .trackId)
+        
+        if let imageUrl = try? container.decode(String.self, forKey: .artworkUrl160) {
+            let largeImage = imageUrl.replacingOccurrences(of: "160x160", with: "300x300")
+            self.artworkUrl160 = URL(string: largeImage)
+        }
     }
 }
 
-enum Explicitness: String, Codable {
-    case cleaned = "cleaned"
-    case explicit = "explicit"
-    case notExplicit = "notExplicit"
-}
-
-enum Country: String, Codable {
-    case usa = "USA"
-}
-
-enum Currency: String, Codable {
-    case usd = "USD"
-}
-
-enum Kind: String, Codable {
-    case featureMovie = "feature-movie"
-    case song = "song"
-    case tvEpisode = "tv-episode"
-}
-
-enum WrapperType: String, Codable, CaseIterable {
-    case audiobook = "audiobook"
-    case track = "track"
-}
