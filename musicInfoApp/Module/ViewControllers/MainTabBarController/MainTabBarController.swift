29df09ddeb8549a4e5c170c9cc593a69c096c791
Palvovets Alexander <alex.pavloveccccc@gmail.com>
2022-06-27 16:22:30 +0300

    feat: update mainTabBarController

--- musicInfoApp/Module/ViewControllers/MainTabBarController/MainTabBarController.swift
+++ musicInfoApp/Module/ViewControllers/MainTabBarController/MainTabBarController.swift
@@ -9,27 +9,43 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
+    
     //MARK: - init
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.makeTabBarController()
+        self.setViewControllers([makeNavigation(viewController: SearchViewController(), title: "Search", image: UIImage(systemName: "magnifyingglass"), isLargeTitle: true, tag: 1), makeNavigation(viewController: SettingViewController(), title: "Setting", image: UIImage(systemName: "gear"), isLargeTitle: true, tag: 2)], animated: true)
         self.tabBarAppearance()
     }
     
-    
-    //MARK: - method 
-    private func generateViewController(view: UIViewController, title: String, image: UIImage?) -> UIViewController {
-        view.title = title
-        view.tabBarItem.image = image
-        return view
+    //MARK: - NavigationController method
+    private func makeNavigation(viewController: UIViewController, title: String? , image: UIImage?, isLargeTitle: Bool, tag: Int) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = isLargeTitle
+        navigationController.tabBarItem = UITabBarItem(title: title, image: image, tag: tag)
+        return navigationController
     }
     
-    private func makeTabBarController() {
-        self.viewControllers = [generateViewController(view: SearchViewController(), title: "Search", image: UIImage(systemName: "magnifyingglass"))]
-    }
-    
+    //MARK: - TO-DO
+    //MARK: - method tabBar
+//    private func generateViewController(view: UIViewController, title: String, image: UIImage?) -> UIViewController {
+//        let navController = UINavigationController(rootViewController: view)
+//        navController.navigationBar.prefersLargeTitles = true
+//        view.title = title
+//        view.tabBarItem.image = image
+//        return view
+//    }
+//
+//    private func makeTabBarController() {
+//        self.viewControllers = [generateViewController(view: SearchViewController(),
+//                                                       title: "Search",
+//                                                       image: UIImage(systemName: "magnifyingglass")),
+//                                generateViewController(view: SettingViewController(),
+//                                                       title: "Setting",
+//                                                       image: UIImage(systemName: "gear"))]
+//    }
+//
     private func tabBarAppearance() {
         tabBar.tintColor = .black
-        tabBar.unselectedItemTintColor = .systemPink
+        tabBar.unselectedItemTintColor = .lightGray
     }
 }
